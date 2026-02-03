9f5eb60fa1108519f67ea351c954b50c50410914
PRN-6 <prinsonroyal1@gmail.com>
2026-02-03 22:58:00 +0530

    added

--- uv_testing/my-backend/main.py
+++ uv_testing/my-backend/main.py
@@ -1,6 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HttpException
+from pydantic import BaseModel 
+from typing import List, Optional
+from datetime import datetime
+import uuid
 
 app = FastAPI()
+
+class User(BaseModel):
+    id: str
+    username: str
+    email: str
+    created_at: datetime
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+
+class post(BaseModel):
+    id: str
+    title: str
+    content: str
+    author_id: str
+    created_at: datetime
+    updated_at: datetime
+
 
 @app.get("/")
 def read_root():
